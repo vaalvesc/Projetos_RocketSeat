caaf4fcae6b9fa3f0371dba84077dfdcfd1c2a46
Victor Alves <victor.alvescosta@outlook.com>
2020-01-22 08:46:34 -0300

    RocketSeat - Iniciando com Backend

---
+++ RocketSeat - Iniciando com Backend/data.js
@@ -0,0 +1,34 @@
+module.exports = [
+
+{   id: "GykTLqODQuU",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "57 min",
+    price: "Free"
+},
+{   id: "HN1UjzRSdBk",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "36 min",
+    price: "Free"
+},
+{   id: "GSqR2i-Pq6o",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "54 min",
+    price: "Free"
+},
+{   id: "x-4z_u8LcGc",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "33 min",
+    price: "Free"
+},
+{   id: "ghTrp1x_1As",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "57 min",
+    price: "Free"
+},
+{   id: "-j7vLmBMsEU",
+    title: "Formulário Animado com JS Puro e CSS animation | Victor Alves",
+    duration: "33 min",
+    price: "Free"
+}
+]
+
---
+++ RocketSeat - Iniciando com Backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/",function(req, res) {
+    return res.render("about")
+})
+
+server.get("/portfolio",function(req, res) {
+    return res.render("portfolio", { items: videos })
+})
+
+server.listen(5000, function() {
+    console.log("server is running")
+})
