e4bf35f4415df32e7fb5c13d40c70f7cfb303d81
Victor Alves <victor.alvescosta@outlook.com>
2020-02-10 21:10:04 -0300

    Novo Projeto Controle Academico

---
+++ RocketSeat -Fase 3 - Controle Academia/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res) {
+    const about = {
+        avatar_url: "https://github.com/vaalvesc/Projetos_RocketSeat/blob/master/Foto_Sobre.jpg?raw=true",
+        name: "Victor Alves",
+        role: "Aprendiz - RocketSeat",
+        description: 'Programador - Full-stack, focado em trazer o melhor ensino para iniciantes em processo de desenvolvimento no <a href="https://www.banesecard.com.br/PortalCliente/Login" target="_blank">banesecard</p>',
+        links: [
+            { name: "Github", url: "https://github.com/vaalvesc/"},
+            { name: "Facebook", url: "https://www.facebook.com/victor.alves810/"},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/victor-antonio-alves-costa-a68a9059/"}
+        ]
+        
+    }
+
+    return res.render("about", { about })
+})
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio", { items: videos })
+})
+
+server.get("/video", function(req, res) {
+    const id = req.query.id
+    
+    const video = videos.find(function(video){
+        if (video.id == id) {
+            return true
+       }
+
+})
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", { item: video })
+})
+
+server.listen(5000, function () {
+    console.log("server is running")
+})
