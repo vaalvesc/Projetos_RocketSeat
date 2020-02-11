71fb2b88864b5a0eeb133d5d9ea50c2fe3497c36
Victor Alves <victor.alvescosta@outlook.com>
2020-02-10 21:38:57 -0300

    reconfigurando arquivo njk

--- RocketSeat -Fase 3 - Controle Academia/routes.js
+++ RocketSeat -Fase 3 - Controle Academia/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-    return res.send("ok")
+    return res.send("about")
 })
 
 module.exports = routes
--- RocketSeat -Fase 3 - Controle Academia/server.js
+++ RocketSeat -Fase 3 - Controle Academia/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const routes = require("/")
+const routes = require("./routes")
+
 const server = express()
 
 server.use(express.static('public'))
+server.use(routes)
 
 server.set("view engine", "njk")
 
